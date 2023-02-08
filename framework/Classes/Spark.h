77c2c750f66301f7f3fe8c9388ff4007d99ef1c9
luisfigueiredoadevinta <124784503+luisfigueiredoadevinta@users.noreply.github.com>
2023-02-08 16:01:57 +0100

    Initial setup of project with GitHub Actions, Xcodegen and Fastlane (#1)
    * initial configuration of repository
    
    * Enable BUILD_LIBRARY_FOR_DISTRIBUTION
    
    * disable ui_tests in githubactions workflow

---
+++ framework/Classes/Spark.h
@@ -0,0 +1,18 @@
+//
+//  Spark.h
+//  Spark
+//
+//  Created by luis.figueiredo-ext on 08/02/2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Spark.
+FOUNDATION_EXPORT double SparkVersionNumber;
+
+//! Project version string for Spark.
+FOUNDATION_EXPORT const unsigned char SparkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Spark/PublicHeader.h>
+
+
