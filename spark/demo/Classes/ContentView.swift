172d212c4acc80062f318840c32afd96c95dbf3e
louis.borlee <louis.borlee@adevinta.com>
2023-02-24 09:28:36 +0100

    Renamed folders + deleted forgotten brands

--- spark/demo/Classes/ContentView.swift
+++ spark/demo/Classes/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, Spark Core Demo!")
+            Text("Hello, Spark Demo!")
         }
         .padding()
     }
---
+++ spark/demo/Classes/SparkDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SparkDemoApp.swift
+//  SparkDemo
+//
+//  Created by luis.figueiredo-ext on 08/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct SparkDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
