68ec8b9b006227a6f425410936491a4d4b12ccc8
Robin Lemaire <robin.lemaire@adevinta.com>
2023-03-10 11:16:23 +0100

    Core: Start Iconography

--- core/Sources/Theming/Content/Iconography/Iconography.swift
+++ core/Sources/Theming/Content/Iconography/Iconography.swift
@@ -5,6 +5,26 @@
 //  Created by louis.borlee on 23/02/2023.
 //
 
-import Foundation
+import UIKit
+import SwiftUI
 
-public protocol Iconography {}
+public protocol Iconography {
+
+}
+
+// MARK: - Style
+
+public protocol IconographyFill {
+    var fill: IconographyImage { get }
+}
+
+public protocol IconographyOutlined {
+    var outlined: IconographyImage { get }
+}
+
+// MARK: - Image
+
+public protocol IconographyImage {
+    var image: UIImage { get }
+    var swiftUIImage: Image { get }
+}
--- core/Sources/Theming/Content/Iconography/IconographyDefault.swift
+++ core/Sources/Theming/Content/Iconography/IconographyDefault.swift
@@ -5,4 +5,52 @@
 //  Created by louis.borlee on 23/02/2023.
 //
 
+import UIKit
+import SwiftUI
+
 public struct IconographyDefault: Iconography {}
+
+// MARK: - Style
+
+public struct IconographyFillDefault: IconographyFill {
+
+    // MARK: - Properties
+
+    public let fill: IconographyImage
+
+    // MARK: - Initialization
+
+    public init(fill: IconographyImage) {
+        self.fill = fill
+    }
+}
+
+public struct IconographyOutlinedDefault: IconographyOutlined {
+
+    // MARK: - Properties
+
+    public let outlined: IconographyImage
+
+    // MARK: - Initialization
+
+    public init(outlined: IconographyImage) {
+        self.outlined = outlined
+    }
+}
+
+// MARK: - Image
+
+public struct IconographyImageDefault: IconographyImage {
+
+    // MARK: - Properties
+
+    public let image: UIImage
+    public let swiftUIImage: Image
+
+    // MARK: - Initialization
+
+    public init(image: UIImage, swiftUIImage: Image) {
+        self.image = image
+        self.swiftUIImage = swiftUIImage
+    }
+}
