07b3ca61d8c1011362be9d005e4ae4a78142d3e9
louis.borlee <louis.borlee@adevinta.com>
2023-03-10 15:21:13 +0100

    Added IconographyCRM + default

---
+++ core/Sources/Theming/Content/Iconography/Sections/CRM/IconographyCRM.swift
@@ -0,0 +1,13 @@
+//
+//  IconographyCRM.swift
+//  SparkCore
+//
+//  Created by louis.borlee on 10/03/2023.
+//
+
+import Foundation
+
+public protocol IconographyCRM {
+    var wallet: IconographyImage { get }
+    var card: IconographyImage { get }
+}
