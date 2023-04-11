da3149d03d8fc7d9771dbaa23f37817012d4e694
Jan-Niklas Freundt <janniklas.freundt.ext@adevinta.com>
2023-04-11 16:59:22 +0200

    Add if modifier.

---
+++ core/Sources/Common/SwiftUI/Modifier/Border/IfModifier.swift
@@ -0,0 +1,19 @@
+//
+//  IfModifier.swift
+//  SparkCore
+//
+//  Created by janniklas.freundt.ext on 06.04.23.
+//  Copyright © 2023 Adevinta. All rights reserved.
+//
+
+import SwiftUI
+
+public extension View {
+    func `if`<Content: View>(_ conditional: Bool, content: (Self) -> Content) -> some View {
+        if conditional {
+            return AnyView(content(self))
+        } else {
+            return AnyView(self)
+        }
+    }
+}
