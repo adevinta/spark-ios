e1687422eb84847b59cb8c1a1614be95cc33ecb7
louis.borlee <louis.borlee@adevinta.com>
2023-04-19 17:45:21 +0200

    Added missing colors assets + added SparkColors tests

---
+++ spark/Sources/Theming/Content/SparkColorsTests.swift
@@ -0,0 +1,51 @@
+//
+//  SparkColorsTests.swift
+//  SparkTests
+//
+//  Created by louis.borlee on 19/04/2023.
+//  Copyright © 2023 Adevinta. All rights reserved.
+//
+
+import XCTest
+import SparkCore
+@testable import Spark
+
+final class SparkColorsTests: XCTestCase {
+
+    private lazy var tokens: [SparkCore.ColorToken] = self.getAllColorTokens()
+
+    func testUIColors() {
+        self.tokens.forEach {
+            XCTAssertNotEqual(
+                $0.uiColor,
+                .clear,
+                "Wrong uiColor for token \($0)"
+            )
+        }
+    }
+
+    func testSwiftUIColors() {
+        self.tokens.forEach {
+            XCTAssertNotEqual(
+                $0.color,
+                .clear,
+                "Wrong color for token \($0)"
+            )
+        }
+    }
+
+    // MARK: - Get Colors
+    private func getAllColorTokens() -> [SparkCore.ColorToken] {
+        let mirror = Mirror(reflecting: SparkColors())
+        return mirror.children.flatMap { (_, value: Any) in
+            return self.getColorTokens(from: value)
+        }
+    }
+
+    private func getColorTokens(from object: Any) -> [SparkCore.ColorToken] {
+        let mirror = Mirror(reflecting: object)
+        return mirror.children.compactMap { (_, value: Any) in
+            return value as? SparkCore.ColorToken
+        }
+    }
+}
