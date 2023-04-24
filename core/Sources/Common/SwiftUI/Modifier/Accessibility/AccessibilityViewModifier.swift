4a921349ccd76b91cb11ea46d6e9d453cc20bcc6
Robin Lemaire <robin.lemaire@adevinta.com>
2023-04-24 17:06:47 +0200

    [Core] SwiftUI: Add accessibility view Modifier

---
+++ core/Sources/Common/SwiftUI/Modifier/Accessibility/AccessibilityViewModifier.swift
@@ -0,0 +1,38 @@
+//
+//  AccessibilityViewModifier.swift
+//  SparkCore
+//
+//  Created by robin.lemaire on 04/04/2023.
+//  Copyright © 2023 Adevinta. All rights reserved.
+//
+
+import SwiftUI
+
+struct AccessibilityViewModifier: ViewModifier {
+
+    // MARK: - Properties
+
+    private let identifier: String
+    private let label: String?
+
+    // MARK: - Initialization
+
+    init(identifier: String?,
+         label: String?) {
+        self.identifier = identifier ?? ""
+        self.label = label
+    }
+
+    // MARK: - View
+
+    func body(content: Content) -> some View {
+        if let label = self.label {
+            content
+                .accessibilityIdentifier(self.identifier)
+                .accessibilityLabel(label)
+        } else {
+            content
+                .accessibilityIdentifier(self.identifier)
+        }
+    }
+}
