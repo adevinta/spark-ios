eb709f88dcac6211763ebcf6b5ef784be63f9232
Michael Zimmermann <michael.zimmermann@adevinta.com>
2023-04-28 18:22:45 +0200

    Radiobutton: Removed snapshot tests.

--- core/Sources/Components/RadioButton/View/RadioButtonViewTests.swift
+++ core/Sources/Components/RadioButton/View/RadioButtonViewTests.swift
@@ -16,58 +16,58 @@ final class RadioButtonViewTests: TestCase {
 
     var boundSelectedID = 0
 
-    func test_multiline_label() throws {
+    func x_test_multiline_label() throws {
         let view = sut(state: .enabled, isSelected: false, label: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.").frame(width: 300, height: 300)
 
         assertSnapshotInDarkAndLight(matching: view)
     }
 
-    func test_enabled_selected() throws {
+    func x_test_enabled_selected() throws {
         let view = sut(state: .enabled, isSelected: true).fixedSize()
         assertSnapshotInDarkAndLight(matching: view)
     }
 
-    func test_enabled_not_selected() throws {
+    func x_test_enabled_not_selected() throws {
         let view = sut(state: .enabled, isSelected: false).fixedSize()
         assertSnapshotInDarkAndLight(matching: view)
     }
 
-    func test_disabled_not_selected() throws {
+    func x_test_disabled_not_selected() throws {
         let view = sut(state: .disabled, isSelected: false).fixedSize()
         assertSnapshotInDarkAndLight(matching: view)
     }
 
-    func test_disabled_selected() throws {
+    func x_test_disabled_selected() throws {
         let view = sut(state: .disabled, isSelected: true).fixedSize()
         assertSnapshotInDarkAndLight(matching: view)
     }
 
-    func test_success_not_selected() throws {
+    func x_test_success_not_selected() throws {
         let view = sut(state: .success(message: "Message"), isSelected: false).fixedSize()
         assertSnapshotInDarkAndLight(matching: view)
     }
 
-    func test_success_selected() throws {
+    func x_test_success_selected() throws {
         let view = sut(state: .success(message: "Message"), isSelected: true).fixedSize()
         assertSnapshotInDarkAndLight(matching: view)
     }
 
-    func test_warning_not_selected() throws {
+    func x_test_warning_not_selected() throws {
         let view = sut(state: .warning(message: "Message"), isSelected: false).fixedSize()
         assertSnapshotInDarkAndLight(matching: view)
     }
 
-    func test_warning_selected() throws {
+    func x_test_warning_selected() throws {
         let view = sut(state: .warning(message: "Message"), isSelected: true).fixedSize()
         assertSnapshotInDarkAndLight(matching: view)
     }
 
-    func test_error_not_selected() throws {
+    func x_test_error_not_selected() throws {
         let view = sut(state: .error(message: "Message"), isSelected: false).fixedSize()
         assertSnapshotInDarkAndLight(matching: view)
     }
 
-    func test_error_selected() throws {
+    func x_test_error_selected() throws {
         let view = sut(state: .error(message: "Message"), isSelected: true).fixedSize()
         assertSnapshotInDarkAndLight(matching: view)
     }
