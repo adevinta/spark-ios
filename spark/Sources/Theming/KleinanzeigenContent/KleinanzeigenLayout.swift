5d73addfde3976a95dbca2186236b7f3fcba61d3
Jan-Niklas Freundt <janniklas.freundt.ext@adevinta.com>
2023-06-08 09:35:52 +0200

    Add Kleinanzeigen theme to demo app.

---
+++ spark/Sources/Theming/KleinanzeigenContent/KleinanzeigenLayout.swift
@@ -0,0 +1,21 @@
+//
+//  KleinanzeigenLayout.swift
+//  Spark
+//
+//  Created by alex.vecherov on 05.06.23.
+//  Copyright © 2023 Adevinta. All rights reserved.
+//
+
+import SparkCore
+
+struct KleinanzeigenLayout: Layout {
+
+    // MARK: - Properties
+
+    let spacing: LayoutSpacing = LayoutSpacingDefault(small: 2,
+                                                      medium: 5,
+                                                      large: 14,
+                                                      xLarge: 19,
+                                                      xxLarge: 27,
+                                                      xxxLarge: 35)
+}
