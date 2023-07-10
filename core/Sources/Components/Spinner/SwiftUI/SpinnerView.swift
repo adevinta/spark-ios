e0d88be61d8180660c80de78228ed459066c0031
Michael Zimmermann <michael.zimmermann@adevinta.com>
2023-07-10 10:12:08 +0200

    [Component] Spinner.

---
+++ core/Sources/Components/Spinner/SwiftUI/SpinnerView.swift
@@ -0,0 +1,16 @@
+//
+//  SpinnerView.swift
+//  SparkCore
+//
+//  Created by michael.zimmermann on 10.07.23.
+//  Copyright © 2023 Adevinta. All rights reserved.
+//
+
+import SwiftUI
+
+struct SpinnerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
---
+++ spark/Demo/Classes/View/Components/Spinner/SpinnerComponent.swift
@@ -0,0 +1,21 @@
+//
+//  SpinnerComponent.swift
+//  SparkDemo
+//
+//  Created by michael.zimmermann on 10.07.23.
+//  Copyright © 2023 Adevinta. All rights reserved.
+//
+
+import SwiftUI
+
+struct SpinnerComponent: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SpinnerComponent_Previews: PreviewProvider {
+    static var previews: some View {
+        SpinnerComponent()
+    }
+}
