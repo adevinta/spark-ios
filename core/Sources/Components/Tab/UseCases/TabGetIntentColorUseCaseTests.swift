27af9d58c731c5e9da39e029f039e9480fe91938
Alican Aycil <alican.aycil@adevinta.com>
2023-07-27 22:49:23 +0200

    [Tab#246] fix tests

--- core/Sources/Components/Tab/UseCases/TabGetIntentColorUseCaseTests.swift
+++ core/Sources/Components/Tab/UseCases/TabGetIntentColorUseCaseTests.swift
@@ -24,10 +24,10 @@ final class TabGetIntentColorUseCaseTests: TestCase {
     
     // MARK: - Tests
     func test_execute_primary() {
-        XCTAssertEqual(self.sut.execute(colors: self.colors, intent: .primary).color, self.colors.primary.primary.color)
+        XCTAssertEqual(self.sut.execute(colors: self.colors, intent: .primary).color, self.colors.main.main.color)
     }
     
     func test_execute_secondary() {
-        XCTAssertEqual(self.sut.execute(colors: self.colors, intent: .secondary).color, self.colors.secondary.secondary.color)
+        XCTAssertEqual(self.sut.execute(colors: self.colors, intent: .secondary).color, self.colors.support.support.color)
     }
 }
