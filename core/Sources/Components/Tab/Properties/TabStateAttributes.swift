bf7d178888e562a448f588978012244b64ab8909
Alican Aycil <alican.aycil@adevinta.com>
2023-07-28 13:56:53 +0200

    [Tab#270] add tab item spacing

--- core/Sources/Components/Tab/Properties/TabStateAttributes.swift
+++ core/Sources/Components/Tab/Properties/TabStateAttributes.swift
@@ -9,24 +9,18 @@
 import Foundation
 
 /// Attributes available for the states of the tab:
-/// - labelColor: defines the color of the text and the tint color of the icon
-/// - lineColor: The color of the base line
-/// - backgroundColor: The background color of the tab item.
+/// - spacings: Spacings of the tab item.
+/// - colors: Colors of the tab item.
 /// - opacity: The opacity of the tab item.
 /// - separatorLineHeight: The lineHeight of the tab item.
 struct TabStateAttributes: Equatable {
     
-    let labelColor: any ColorToken
-    let lineColor: any ColorToken
-    let backgroundColor: any ColorToken
+    let spacings: TabItemSpacings
+    let colors: TabItemColors
     let opacity: CGFloat?
     let separatorLineHeight: CGFloat
     
     static func == (lhs: TabStateAttributes, rhs: TabStateAttributes) -> Bool {
-        return colorsEqual(lhs.labelColor, rhs.labelColor) && colorsEqual(lhs.lineColor, rhs.lineColor) && colorsEqual(lhs.backgroundColor, rhs.backgroundColor) && lhs.opacity == rhs.opacity && lhs.separatorLineHeight == rhs.separatorLineHeight
-    }
-    
-    private static func colorsEqual(_ lhs: any ColorToken, _ rhs: any ColorToken) -> Bool {
-        return lhs.color == rhs.color && lhs.uiColor == rhs.uiColor
+        return lhs.spacings == rhs.spacings && lhs.colors == rhs.colors && lhs.opacity == rhs.opacity && lhs.separatorLineHeight == rhs.separatorLineHeight
     }
 }
