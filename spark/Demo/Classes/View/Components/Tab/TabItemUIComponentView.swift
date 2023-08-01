9395d2e7ba4824f4b02c1c12c26f0281d4382b13
Michael Zimmermann <michael.zimmermann@adevinta.com>
2023-08-01 13:34:36 +0200

    [Tab#275] Added Component to DemoApp.

--- spark/Demo/Classes/View/Components/Tab/TabItemUIComponentView.swift
+++ spark/Demo/Classes/View/Components/Tab/TabItemUIComponentView.swift
@@ -19,46 +19,76 @@ struct TabItemUIComponentView: View {
             intent: .danger,
             value: 5,
             isBorderVisible: false)
-
         TabItemUIComponentRepresentableView(
             theme: themePublisher.theme,
+            intent: .main,
             label: "Tab 1",
             icon: UIImage(systemName: "fleuron.fill"),
-            badge: badge
+            badge: badge,
+            isSelected: true,
+            isEnabled: true
         )
+
     }
 }
 
 struct TabItemUIComponentRepresentableView: UIViewRepresentable {
     let theme: Theme
-    let label: String
+    let label: String?
     let icon: UIImage?
     let badge: BadgeUIView?
+    let intent: TabIntent
+    let isSelected: Bool
+    let isEnabled: Bool
 
     init(theme: Theme,
-         label: String,
+         intent: TabIntent,
+         label: String?,
          icon: UIImage?,
-         badge: BadgeUIView?) {
+         badge: BadgeUIView?,
+         isSelected: Bool,
+         isEnabled: Bool
+    ) {
         self.theme = theme
+        self.intent = intent
         self.label = label
         self.icon = icon
         self.badge = badge
+        self.isSelected = isSelected
+        self.isEnabled = isEnabled
     }
 
     func makeUIView(context: Context) -> SparkCore.TabItemUIView {
-        let view = TabItemUIView(theme: self.theme, label: self.label)
+        let view = TabItemUIView(
+            theme: self.theme,
+            intent: self.intent,
+            label: self.label,
+            icon: self.icon)
+
         view.icon = self.icon
         view.badge = self.badge
+        view.isSelected = self.isSelected
+        view.isEnabled = self.isEnabled
 
         return view
     }
 
     func updateUIView(_ uiView: SparkCore.TabItemUIView, context: Context) {
+        uiView.theme = self.theme
+        uiView.intent = self.intent
+        uiView.badge = self.badge
+        uiView.icon = self.icon
+        uiView.text = self.label
+        uiView.isEnabled = self.isEnabled
+        uiView.isSelected = self.isSelected
     }
 }
 
 struct TabItemUIComponentView_Previews: PreviewProvider {
     static var previews: some View {
-        TabItemUIComponentView()
+        VStack {
+            TabItemUIComponentView()
+            Text("Hello World")
+        }
     }
 }
