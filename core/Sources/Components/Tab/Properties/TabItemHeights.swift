502287f7db5627c2980cc81d0cd1b2fafc705e66
Michael Zimmermann <michael.zimmermann@adevinta.com>
2023-08-08 17:22:59 +0200

    [Build] Defined fixed height for tab item.

---
+++ core/Sources/Components/Tab/Properties/TabItemHeights.swift
@@ -0,0 +1,14 @@
+//
+//  TabItemHeights.swift
+//  SparkCore
+//
+//  Created by michael.zimmermann on 08.08.23.
+//  Copyright © 2023 Adevinta. All rights reserved.
+//
+
+import Foundation
+
+struct TabItemHeights: Equatable, Updateable {
+    var separatorLineHeight: CGFloat
+    var itemHeight: CGFloat
+}
