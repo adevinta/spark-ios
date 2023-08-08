11501c081a70286c80877f843308a76ebff372bd
Michael Zimmermann <michael.zimmermann@adevinta.com>
2023-08-08 14:27:04 +0200

    [Tab#275] Removed button. Renamed label to text.

--- spark/Demo/Classes/View/Components/Tab/TabItemComponent.swift
+++ spark/Demo/Classes/View/Components/Tab/TabItemComponent.swift
@@ -144,7 +144,7 @@ struct TabItemComponent: View {
                         theme: self.theme,
                         intent: self.intent,
                         tabSize: self.tabSize,
-                        label: self.showText.isSelected ? "Label" : nil,
+                        text: self.showText.isSelected ? "Label" : nil,
                         icon: self.showIcon.isSelected ? UIImage(systemName: "fleuron.fill") : nil,
                         badge: self.showBadge.isSelected ? badge : nil,
                         isSelected: self.isSelected.isSelected,
