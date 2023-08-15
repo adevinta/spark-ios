6e51f643681f2802303ee3264f9504ceb6419dc2
Michael Zimmermann <michael.zimmermann@adevinta.com>
2023-08-15 10:40:02 +0200

    [Checkbox#1271] Added default accessibility identifier.

---
+++ core/Sources/Components/Checkbox/AccessibilityIdentifier/CheckboxAccessibilityIdentifier.swift
@@ -0,0 +1,15 @@
+//
+//  CheckboxAccessibilityIdentifier.swift
+//  SparkCore
+//
+//  Created by michael.zimmermann on 15.08.23.
+//  Copyright © 2023 Adevinta. All rights reserved.
+//
+
+import Foundation
+
+/// The accessibility identifiers for the checkbox.
+public enum CheckboxAccessibilityIdentifier {
+    /// The default accessibility identifier. Can be changed by the consumer
+    public static let checkbox = "spark-check-box"
+}
