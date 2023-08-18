fee89d480f9e988c3034cb3357226bd099c7cb1b
Alican Aycil <alican.aycil@adevinta.com>
2023-08-18 20:27:13 +0200

    [Tech#319] Add badge component and spark action sheet

--- spark/Demo/SceneDelegate.swift
+++ spark/Demo/SceneDelegate.swift
@@ -16,7 +16,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = scene as? UIWindowScene else { return }
 
         let window = UIWindow(windowScene: windowScene)
-        window.rootViewController = SparkDemoTabbarController()
+        window.rootViewController = SparkTabbarController()
         self.window = window
         window.makeKeyAndVisible()
     }
