9b544ca926422c5cae30cec5ae9e9a78a12332d1
Michael Zimmermann <michael.zimmermann@adevinta.com>
2023-09-01 11:32:56 +0200

    [Tab#396] Set background color of scroll view.

--- core/Sources/Components/Tab/UseCases/TabsGetAttributesUseCase.swift
+++ core/Sources/Components/Tab/UseCases/TabsGetAttributesUseCase.swift
@@ -8,14 +8,37 @@
 
 import Foundation
 
+// sourcery: AutoMockable
 protocol TabsGetAttributesUseCaseable {
-    func execute(theme: Theme) -> TabsAttributes
+    func execute(theme: Theme, isEnabled: Bool) -> TabsAttributes
 }
 
-struct TabsGetAttributesUseCase {
-    func execute(theme: Theme) -> TabsAttributes {
+/// TabGetColorUseCase
+/// Use case to determin the colors of the Tab by the intent
+/// Functions:
+/// - execute: returns a color token for given colors and an intent
+struct TabsGetAttributesUseCase: TabsGetAttributesUseCaseable {
+
+    // MARK: - Functions
+    ///
+    /// Calculate the attributes of the tab control depending on wheter it is enabled or not.
+    ///
+    /// - Parameters:
+    ///    - theme: The theme
+    ///    - isEnabled: Bool.
+    ///
+    /// - Returns: ``TabsAttributes`` containing colors and line hight of the tab control.
+    func execute(theme: Theme, isEnabled: Bool) -> TabsAttributes {
+
+        var lineColor = theme.colors.base.outline
+        if !isEnabled {
+            lineColor = lineColor.opacity(theme.dims.dim3)
+        }
+
         return TabsAttributes(
             lineHeight: theme.border.width.small,
-            lineColor: theme.colors.base.outline)
+            lineColor: lineColor,
+            backgroundColor: theme.colors.base.surface
+        )
     }
 }
