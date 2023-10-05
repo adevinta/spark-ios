8dd7ea9346869258dc4fa3b6ea374a0a7bb9ed2a
Alican Aycil <alican.aycil@adevinta.com>
2023-10-05 10:46:36 +0200

    [Checkbox#329] fix merged conflicts

--- spark/Demo/Classes/View/Components/Button/SwiftUI/ButtonComponentView.swift
+++ spark/Demo/Classes/View/Components/Button/SwiftUI/ButtonComponentView.swift
@@ -86,7 +86,7 @@ struct ButtonComponentView: View {
                     text: "Is enabled",
                     checkedImage: DemoIconography.shared.checkmark,
                     theme: self.theme,
-                    state: .enabled,
+                    isEnabled: true,
                     selectionState: self.$isEnabled
                 )
             },
