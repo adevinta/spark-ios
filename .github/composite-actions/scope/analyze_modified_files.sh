56d004988aea29a5ea056e5b0d317452730710a3
solygambas <51904909+solygambas@users.noreply.github.com>
2023-10-17 12:04:55 +0200

    ci: add test classe without suite

--- .github/composite-actions/scope/analyze_modified_files.sh
+++ .github/composite-actions/scope/analyze_modified_files.sh
@@ -3,14 +3,8 @@
 # Get modified files list from command-line arguments
 MODIFIED_FILES="$@"
 
-# echo "Simulated modified files:"
-# echo "$MODIFIED_FILES"
-
 # Extract modified component scopes
 COMPONENT_SCOPES=$(echo "$MODIFIED_FILES" | sed -n 's#.*Components/\([^/]*\)/.*#\1#p' | sort -u)
-
-# echo "Modified component scopes:"
-# echo "$COMPONENT_SCOPES"
 
 # Initialize lists for test classes
 SWIFTUI_TESTS=""
@@ -26,10 +20,6 @@ for COMPONENT in $COMPONENT_SCOPES; do
         UIKIT_TESTS="$UIKIT_TESTS ${COMPONENT}UIViewSnapshotTests"
     fi
 done
-
-# Print the lists of test classes
-# echo "SwiftUI Test Classes: $SWIFTUI_TESTS"
-# echo "UIKit Test Classes: $UIKIT_TESTS"
 
 # Combine SwiftUI and UIKit test classes into a single list
 ALL_TESTS="$SWIFTUI_TESTS $UIKIT_TESTS"
