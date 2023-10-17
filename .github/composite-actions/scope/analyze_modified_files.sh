61a1a2d2c701e678144ca541a8753c672e80da91
solygambas <51904909+solygambas@users.noreply.github.com>
2023-10-17 12:21:35 +0200

    ci: fix test array

--- .github/composite-actions/scope/analyze_modified_files.sh
+++ .github/composite-actions/scope/analyze_modified_files.sh
@@ -6,6 +6,9 @@ MODIFIED_FILES="$@"
 # Extract modified component scopes
 COMPONENT_SCOPES=$(echo "$MODIFIED_FILES" | sed -n 's#.*Components/\([^/]*\)/.*#\1#p' | sort -u)
 
+# Define test suite name
+TEST_SUITE="SparkCoreSnapshotTests"
+
 # Initialize lists for test classes
 SWIFTUI_TESTS=""
 UIKIT_TESTS=""
@@ -13,16 +16,16 @@ UIKIT_TESTS=""
 # Analyze subfolders under View folder and add test classes to respective lists
 for COMPONENT in $COMPONENT_SCOPES; do
     if grep -q "core/Sources/Components/$COMPONENT/View/SwiftUI/" <<< "$MODIFIED_FILES"; then
-        SWIFTUI_TESTS="$SWIFTUI_TESTS ${COMPONENT}ViewSnapshotTests"
+        SWIFTUI_TESTS="${SWIFTUI_TESTS:+$SWIFTUI_TESTS,}$TEST_SUITE/${COMPONENT}ViewSnapshotTests"
     fi
 
     if grep -q "core/Sources/Components/$COMPONENT/View/UIKit/" <<< "$MODIFIED_FILES"; then
-        UIKIT_TESTS="$UIKIT_TESTS ${COMPONENT}UIViewSnapshotTests"
+        UIKIT_TESTS="${UIKIT_TESTS:+$UIKIT_TESTS,}$TEST_SUITE/${COMPONENT}UIViewSnapshotTests"
     fi
 done
 
 # Combine SwiftUI and UIKit test classes into a single list
-ALL_TESTS="$SWIFTUI_TESTS $UIKIT_TESTS"
+ALL_TESTS="$SWIFTUI_TESTS,$UIKIT_TESTS"
 
 # Print the combined list of test classes
 echo "$ALL_TESTS"
