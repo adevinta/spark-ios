336f82d3db5650c027bafd1db7dde155db5a62ef
Michael Zimmermann <michael.zimmermann@adevinta.com>
2023-11-20 16:30:55 +0100

    [Rating#591] Added snapshot tests.

---
+++ core/Sources/Components/Rating/View/UIKit/RatingDisplayUIViewSnapshotTests.swift
@@ -0,0 +1,44 @@
+//
+//  RatingDisplayUIViewSnapshotTests.swift
+//  Spark
+//
+//  Created by Michael Zimmermann on 20.11.23.
+//  Copyright © 2023 Adevinta. All rights reserved.
+//
+
+import Foundation
+
+@testable import SparkCore
+
+final class RatingDisplayUIViewSnapshotTests: UIKitComponentSnapshotTestCase {
+
+    // MARK: - Properties
+
+    private let theme: Theme = SparkTheme.shared
+
+    // MARK: - Tests
+
+    func test() {
+        let scenarios = RatingDisplayScenarioSnapshotTests.allCases
+
+        for scenario in scenarios {
+            let configurations = scenario.configuration(isSwiftUIComponent: false)
+            for configuration in configurations {
+                let view = RatingDisplayUIView(
+                    theme: self.theme,
+                    intent: .main,
+                    count: configuration.count,
+                    size: configuration.size,
+                    rating: configuration.rating
+                )
+
+                self.assertSnapshot(
+                    matching: view,
+                    modes: configuration.modes,
+                    sizes: configuration.sizes,
+                    testName: configuration.testName()
+                )
+            }
+        }
+    }
+}
