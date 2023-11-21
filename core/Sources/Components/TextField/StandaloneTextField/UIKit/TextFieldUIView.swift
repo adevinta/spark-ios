6ba01825db581d2dc859c0f500c93f8aeede72ba
Jacklyn Situmorang <jacklyn.situmorang@adevinta.com>
2023-11-21 15:17:28 +0100

    [TextField#278] Set border color to Base/OutlineHigh when textfield is active

--- core/Sources/Components/TextField/StandaloneTextField/UIKit/TextFieldUIView.swift
+++ core/Sources/Components/TextField/StandaloneTextField/UIKit/TextFieldUIView.swift
@@ -169,4 +169,20 @@ public final class TextFieldUIView: UITextField {
         self._height.update(traitCollection: traitCollection)
         self.updateHeight()
     }
+
+    // MARK: - Instance methods
+
+    public override func becomeFirstResponder() -> Bool {
+        let result = super.becomeFirstResponder()
+        self.setBorderColor(from: self.theme.colors.base.outlineHigh)
+        return result
+    }
+
+    public override func resignFirstResponder() -> Bool {
+        let result = super.resignFirstResponder()
+        self.setBorderColor(from: self.viewModel.colors.border)
+        return result
+    }
+
+
 }
