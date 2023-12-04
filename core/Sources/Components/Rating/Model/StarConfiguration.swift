e6ed7b81bbf62428005336a1089b03a6ee708731
louis.borlee <louis.borlee@adevinta.com>
2023-12-04 14:21:13 +0100

    [RatingsDisplay] Copied RatingsDisplay to make a clean release of this component (from 0.8.0)

---
+++ core/Sources/Components/Rating/Model/StarConfiguration.swift
@@ -0,0 +1,36 @@
+//
+//  StarConfiguration.swift
+//  SparkCore
+//
+//  Created by Michael Zimmermann on 17.11.23.
+//  Copyright © 2023 Adevinta. All rights reserved.
+//
+
+import Foundation
+
+public struct StarConfiguration: Equatable {
+    public var numberOfVertices: Int
+    public var vertexSize: CGFloat
+    public var cornerRadiusSize: CGFloat
+
+    // MARK: - Default values
+    public enum Defaults {
+        public static let numberOfVertices = 5
+        public static let vertexSize = CGFloat(0.65)
+        public static let cornerRadiusSize = CGFloat(0.15)
+    }
+    
+    public static let `default` = StarConfiguration(
+        numberOfVertices: Defaults.numberOfVertices,
+        vertexSize: Defaults.vertexSize,
+        cornerRadiusSize: Defaults.cornerRadiusSize)
+    
+    public init(numberOfVertices: Int,
+         vertexSize: CGFloat,
+         cornerRadiusSize: CGFloat) {
+        self.numberOfVertices = numberOfVertices
+        self.vertexSize = vertexSize
+        self.cornerRadiusSize = cornerRadiusSize
+    }
+    
+}
