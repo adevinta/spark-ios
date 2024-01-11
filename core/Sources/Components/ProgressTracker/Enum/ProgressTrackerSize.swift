d19be4a31d01ff9c04a462dbfd4314d41af10ca6
Michael Zimmermann <michael.zimmermann@adevinta.com>
2024-01-11 18:01:35 +0100

    [ProgressTracker#740] Added use cases.

---
+++ core/Sources/Components/ProgressTracker/Enum/ProgressTrackerSize.swift
@@ -0,0 +1,15 @@
+//
+//  ProgressTrackerSize.swift
+//  SparkCore
+//
+//  Created by Michael Zimmermann on 11.01.24.
+//  Copyright © 2024 Adevinta. All rights reserved.
+//
+
+import Foundation
+
+public enum ProgressTrackerSize: CGFloat {
+    case small = 16
+    case medium = 24
+    case large = 32
+}
---
+++ core/Sources/Components/ProgressTracker/Enum/ProgressTrackerVariant.swift
@@ -0,0 +1,14 @@
+//
+//  ProgressTrackerVariant.swift
+//  SparkCore
+//
+//  Created by Michael Zimmermann on 11.01.24.
+//  Copyright © 2024 Adevinta. All rights reserved.
+//
+
+import Foundation
+
+public enum ProgressTrackerVariant: CaseIterable {
+    case outlined
+    case tinted
+}
