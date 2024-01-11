113861dd6bd748c1f43fa75d7bfca13ca935cebb
Michael Zimmermann <michael.zimmermann@adevinta.com>
2024-01-11 18:01:35 +0100

    [ProgressTracker#740] Added use cases.

---
+++ core/Sources/Components/ProgressTracker/Enum/ProgressTrackerIntent.swift
@@ -0,0 +1,21 @@
+//
+//  ProgressTrackerIntent.swift
+//  SparkCore
+//
+//  Created by Michael Zimmermann on 11.01.24.
+//  Copyright © 2024 Adevinta. All rights reserved.
+//
+
+import Foundation
+
+public enum ProgressTrackerIntent {
+    case accent
+    case alert
+    case basic
+    case danger
+    case info
+    case main
+    case neutral
+    case success
+    case support
+}
