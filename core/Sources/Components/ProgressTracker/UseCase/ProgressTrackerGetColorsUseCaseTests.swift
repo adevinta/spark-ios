9d5b569c698663b8ccdf258181b172b2fb3c32ca
Michael Zimmermann <michael.zimmermann@adevinta.com>
2024-01-18 16:45:11 +0100

    [ProgressTracker#740] Added unit tests.

---
+++ core/Sources/Components/ProgressTracker/UseCase/ProgressTrackerGetColorsUseCaseTests.swift
@@ -0,0 +1,111 @@
+//
+//  ProgressTrackerGetColorsUseCaseTests.swift
+//  SparkCoreUnitTests
+//
+//  Created by Michael Zimmermann on 18.01.24.
+//  Copyright © 2024 Adevinta. All rights reserved.
+//
+
+import Foundation
+import XCTest
+
+@testable import SparkCore
+
+final class ProgressTrackerGetColorsUseCaseTests: XCTestCase {
+
+    var sut: ProgressTrackerGetColorsUseCase!
+    var theme: ThemeGeneratedMock!
+    var outlinedUseCase: ProgressTrackerGetVariantColorsUseCaseableGeneratedMock!
+    var tintedUseCase: ProgressTrackerGetVariantColorsUseCaseableGeneratedMock!
+
+    // MARK: - Setup
+    override func setUp() {
+        super.setUp()
+        self.theme = ThemeGeneratedMock.mocked()
+        self.outlinedUseCase = ProgressTrackerGetVariantColorsUseCaseableGeneratedMock()
+        self.tintedUseCase = ProgressTrackerGetVariantColorsUseCaseableGeneratedMock()
+        let sut = ProgressTrackerGetColorsUseCase(
+            getTintedColorsUseCase: self.tintedUseCase,
+            getOutlinedColorsUseCase: self.outlinedUseCase
+        )
+
+        self.sut = sut
+    }
+
+    // MARK: - Tests
+    func test_tinted_colors() {
+        // GIVEN
+        let colors = self.theme.colors
+        let expectedColors = ProgressTrackerColors(
+            background: colors.main.mainContainer,
+            outline: colors.main.mainContainer,
+            content: colors.main.onMainContainer,
+            label: colors.basic.basic)
+
+        self.tintedUseCase.executeWithColorsAndIntentAndStateReturnValue = expectedColors
+
+        // WHEN
+        let tabColors = self.sut.execute(
+            theme: self.theme,
+            intent: .basic,
+            variant: .tinted,
+            state: .default)
+
+        // THEN
+        XCTAssertEqual(tabColors, expectedColors)
+    }
+
+    func test_outlined_colors() {
+        // GIVEN
+        let colors = self.theme.colors
+        let expectedColors = ProgressTrackerColors(
+            background: colors.main.mainContainer,
+            outline: colors.main.main,
+            content: colors.main.main,
+            label: colors.basic.basic)
+
+        self.outlinedUseCase.executeWithColorsAndIntentAndStateReturnValue = expectedColors
+
+        // WHEN
+        let tabColors = self.sut.execute(
+            theme: self.theme,
+            intent: .basic,
+            variant: .outlined,
+            state: .default)
+
+        // THEN
+        XCTAssertEqual(tabColors, expectedColors)
+    }
+
+    func test_colors_disabled() {
+        // GIVEN
+        let colors = self.theme.colors
+        let dims = self.theme.dims
+
+        let useCaseColors = ProgressTrackerColors(
+            background: colors.main.mainContainer,
+            outline: colors.main.main,
+            content: colors.main.main,
+            label: colors.basic.basic)
+
+        let expectedColors = ProgressTrackerColors(
+            background: useCaseColors.background.opacity(dims.dim2),
+            outline: useCaseColors.outline.opacity(dims.dim2),
+            content: useCaseColors.content.opacity(dims.dim2),
+            label: useCaseColors.label.opacity(dims.dim1)
+        )
+
+        self.outlinedUseCase.executeWithColorsAndIntentAndStateReturnValue = useCaseColors
+
+        // WHEN
+        let tabColors = self.sut.execute(
+            theme: self.theme,
+            intent: .basic,
+            variant: .outlined,
+            state: .disabled)
+
+        // THEN
+        XCTAssertEqual(tabColors, expectedColors)
+    }
+
+}
