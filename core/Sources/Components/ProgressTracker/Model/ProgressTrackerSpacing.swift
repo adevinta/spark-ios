bb158cc1579088c1ae7c79f4c22328aa0a94b354
Michael Zimmermann <michael.zimmermann@adevinta.com>
2024-01-24 17:16:51 +0100

    [ProgressTracker#741] Add view models and more use cases.

---
+++ core/Sources/Components/ProgressTracker/Model/ProgressTrackerSpacing.swift
@@ -0,0 +1,14 @@
+//
+//  ProgressTrackerSpacing.swift
+//  SparkCore
+//
+//  Created by Michael Zimmermann on 24.01.24.
+//  Copyright © 2024 Adevinta. All rights reserved.
+//
+
+import Foundation
+
+struct ProgressTrackerSpacing: Updateable, Equatable {
+    var trackIndicatorSpacing: CGFloat
+    var minLabelSpacing: CGFloat
+}
