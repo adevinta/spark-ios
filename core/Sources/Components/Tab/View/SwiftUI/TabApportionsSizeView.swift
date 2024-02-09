ba954f66d4c139f0f469143625d41523fac3814d
Michael Zimmermann <michael.zimmermann@adevinta.com>
2024-02-09 10:30:16 +0100

    Revert "[Tab#746] Fix freezing of scroll view when single tab disabled."
    This reverts commit 7b4a543c04a120ded1985398fdbcf3ebf2834ac6.

--- core/Sources/Components/Tab/View/SwiftUI/TabApportionsSizeView.swift
+++ core/Sources/Components/Tab/View/SwiftUI/TabApportionsSizeView.swift
@@ -86,9 +86,6 @@ struct TabApportionsSizeView: View {
                 Spacer()
             }
         }
-        .onChange(of: self.viewModel.content) { _ in
-            self.apportionsContentWidth = 0
-        }
     }
 
     @ViewBuilder
@@ -99,8 +96,7 @@ struct TabApportionsSizeView: View {
             content: content,
             proxy: proxy,
             selectedIndex: self.$selectedIndex,
-            index: index)        
-        .background {
+            index: index)        .background {
                 GeometryReader { geometry in
                     Color.clear
                         .onAppear {
@@ -114,7 +110,11 @@ struct TabApportionsSizeView: View {
     }
 
     private func updateApportionsContentWidth(_ width: CGFloat, index: Int) {
-        self.apportionsContentWidth += width
+        if index == self.viewModel.content.count - 1 {
+            self.apportionsContentWidth = width
+        } else {
+            self.apportionsContentWidth += width
+        }
         self.updateAxis()
     }
 
