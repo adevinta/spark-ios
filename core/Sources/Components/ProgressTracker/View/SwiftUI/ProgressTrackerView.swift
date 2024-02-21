f0c4aa42543ca8b21d026d166b49a1dbc517d937
Michael Zimmermann <michael.zimmermann@adevinta.com>
2024-02-21 10:58:43 +0100

    [ProgressTracker#743] Added further configuration.

--- core/Sources/Components/ProgressTracker/View/SwiftUI/ProgressTrackerView.swift
+++ core/Sources/Components/ProgressTracker/View/SwiftUI/ProgressTrackerView.swift
@@ -73,6 +73,15 @@ public struct ProgressTrackerView: View {
     }
 
     public var body: some View {
+        self.progressTrackerView
+            .isEnabledChanged { isEnabled in
+                print("PT IS ENABLED \(isEnabled)")
+                self.viewModel.isEnabled = isEnabled
+            }
+    }
+
+    @ViewBuilder
+    private var progressTrackerView: some View {
         if self.viewModel.orientation == .horizontal {
             ProgressTrackerHorizontalView(intent: self.intent, variant: self.variant, size: self.size, currentPageIndex: self.$currentPageIndex, viewModel: self.viewModel)
         } else {
@@ -80,8 +89,82 @@ public struct ProgressTrackerView: View {
         }
     }
 
+    // MARK: - Public modifiers
     public func useFullWidth(_ fullWidth: Bool) -> Self {
         self.viewModel.useFullWidth = fullWidth
+        return self
+    }
+
+    /// Set the indicator image at the specified index
+    /// - Parameters:
+    ///   - image: An optional image. Setting the image to nil will remove it.
+    ///   - forIndex: The index to use the image
+    public func indicatorImage(_ image: Image?, forIndex index: Int) -> Self {
+        self.viewModel.content.setIndicatorImage(image, atIndex: index)
+        return self
+    }
+
+    /// Set the current indicator image at the given index. This indicator image will be shown when the page is selected
+    /// - Parameters:
+    ///   - image: An optional image. Setting the image to nil will remove it
+    ///   - forIndex: The page index for the image
+    public func currentPageIndicatorImage(_ image: Image?, forIndex index: Int) -> Self {
+        self.viewModel.content.setCurrentPageIndicatorImage(image, atIndex: index)
+        return self
+    }
+
+    /// Set an attributed label aligned to the corresponding indicator. This will be below the indicator in a horizontal alignment and to the right of it in a vertical alignment. Setting an attributed label and label are mutually exclusive. Setting a label at the position of an attributed label will overwrite the attributed label.
+    /// - Parameters:
+    ///   - attributedLabel: An optional attributed label to set at the given index. Setting this value to nil will remove an existing attributedLabel or label at the index.
+    ///   - forIndex: The index of the label
+    public func attributedLabel(_ attributedLabel: AttributedString?, forIndex index: Int) -> Self {
+        self.viewModel.content.setAttributedLabel(attributedLabel, atIndex: index)
+        return self
+    }
+
+    /// Set a label at the corresponding index. This will overwrite an existing attributed label at the same position.
+    /// - Parameters:
+    ///   - label: An optional label. Setting it to nil, will remove an existing label or attributed label.
+    ///   - index: The page index
+    public func label(_ label: String?, forIndex index: Int) -> Self {
+        let attributedLabel = label.map(AttributedString.init)
+        self.viewModel.content.setAttributedLabel(attributedLabel, atIndex: index)
+        return self
+    }
+
+    /// Set a character on the indicator for the given index.
+    /// - Parameters:
+    ///   - label: An optional character for the indicator label
+    ///   - forIndex: The index of the indicator
+    public func indicatorLabel(_ label: String?, forIndex index: Int) -> Self {
+        self.viewModel.content.setIndicatorLabel(label, atIndex: index)
+        return self
+    }
+
+    /// Set the indicator image of the already visited pages
+    public func completedIndicatorImage(_ image: Image?) -> Self {
+        self.viewModel.content.completedPageIndicatorImage = image
+        return self
+    }
+
+    /// Set the indicator at
+    public func disable(_ isDisabled: Bool, forIndex index: Int) -> Self {
+        guard index < self.viewModel.numberOfPages else { return self }
+
+        self.viewModel.setIsEnabled(isEnabled: !isDisabled, forIndex: index)
+        return self
+    }
+
+    /// Set the default preferred indicator image
+    public func preferredIndicatorImage(_ image: Image?) -> Self {
+        self.viewModel.content.preferredIndicatorImage = image
+
+        return self
+    }
+
+    /// Set the default image for the current page indicator
+    public func preferredCurrentPageIndicatorImage(_ image: Image?) -> Self {
+        self.viewModel.content.preferredCurrentPageIndicatorImage = image
         return self
     }
 }
