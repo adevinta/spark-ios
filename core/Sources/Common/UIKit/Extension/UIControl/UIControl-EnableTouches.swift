dae7ab31b7ebcb23f20616e35157c49a062941f9
Michael Zimmermann <michael.zimmermann@adevinta.com>
2024-02-21 18:29:06 +0100

    [ProgressTracker#824] Fix touch when parent has gesture recognizer.

--- core/Sources/Common/UIKit/Extension/UIControl/UIControl-EnableTouches.swift
+++ core/Sources/Common/UIKit/Extension/UIControl/UIControl-EnableTouches.swift
@@ -9,10 +9,16 @@
 import UIKit
 
 extension UIControl {
+<<<<<<< HEAD
 
     // Add a default tap gesture recognizer without any action to detect the action/publisher/target action even if the parent view has a gesture recognizer
     // Why ? UIControl action/publisher/target doesn't work if the parent contains a gesture recognizer.
     // Note: Native UIButton add the same default recognizer to manage this use case.
+=======
+    //Add a default tap gesture recognizer without any action to detect the action/publisher/target action even if the parent view has a gesture recognizer
+    //Why ? UIControl action/publisher/target doesn't work if the parent contains a gesture recognizer.
+    //Note: Native UIButton add the same default recognizer to manage this use case.
+>>>>>>> 7963ca03 ([ProgressTracker#824] Fix touch when parent has gesture recognizer.)
     func enableTouch() {
         let gestureRecognizer = UITapGestureRecognizer()
         gestureRecognizer.cancelsTouchesInView = false
