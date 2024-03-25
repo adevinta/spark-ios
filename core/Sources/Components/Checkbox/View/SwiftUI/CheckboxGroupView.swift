5cd4a6055cc6ab406be2c2c57e3081cfbbd31403
Alican Aycil <alican.aycil@adevinta.com>
2024-03-25 17:47:01 +0100

    [CheckboxGroup#864] Fix partially disable issue on swiftui checkbox group

--- core/Sources/Components/Checkbox/View/SwiftUI/CheckboxGroupView.swift
+++ core/Sources/Components/Checkbox/View/SwiftUI/CheckboxGroupView.swift
@@ -194,6 +194,7 @@ public struct CheckboxGroupView: View {
             isEnabled: item.isEnabled.wrappedValue,
             selectionState: item.selectionState
         )
+        .disabled(!item.isEnabled.wrappedValue)
         .accessibilityIdentifier(CheckboxAccessibilityIdentifier.checkboxGroupItem(item.id.wrappedValue))
     }
 }
