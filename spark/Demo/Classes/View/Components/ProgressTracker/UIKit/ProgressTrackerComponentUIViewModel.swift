16240732e2d7975f8f8e4a745f3927d0091b1528
Michael Zimmermann <michael.zimmermann@adevinta.com>
2024-04-12 19:36:14 +0200

    [ProgressTracker#890] Add accessibility identifiers.

--- spark/Demo/Classes/View/Components/ProgressTracker/UIKit/ProgressTrackerComponentUIViewModel.swift
+++ spark/Demo/Classes/View/Components/ProgressTracker/UIKit/ProgressTrackerComponentUIViewModel.swift
@@ -234,7 +234,7 @@ final class ProgressTrackerComponentUIViewModel: ComponentUIViewModel {
     // MARK: - Initialization
     @Published var theme: Theme
     @Published var intent: ProgressTrackerIntent = .basic
-    @Published var orientation: ProgressTrackerOrientation = .horizontal
+    @Published var orientation: ProgressTrackerOrientation = .vertical
     @Published var variant: ProgressTrackerVariant = .outlined
     @Published var size: ProgressTrackerSize
     @Published var interaction: ProgressTrackerInteractionState
