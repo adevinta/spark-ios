4ce7747bd4999fb8d4d6935512a066f902fea9ce
Michael Zimmermann <michael.zimmermann@adevinta.com>
2024-06-25 11:13:40 +0200

    [IRP] Reorganise script.

--- spm-components.sh
+++ spm-components.sh
@@ -1,5 +1,7 @@
 #/bin/sh
 
+# find . -type d -empty | xargs git rm
+
 pp () {
    IFS='/' read -r -a array <<< "$1"
    folder=${array[0]}
@@ -16,41 +18,41 @@ pp () {
      fp=`echo "${file%/*}/"`
      if [ -z "${file##*+ExtensionTests.swift}" ]; then
         if ! [ -d "${folder}/Sources/Testing/${fp}" ]; then
-           echo "mkdir ${folder}/Sources/Testing/${fp}"
+           mkdir -p ${folder}/Sources/Testing/${fp}
         fi
-        echo "git add ${folder}/Sources/Testing/${fp}"
-        echo "git mv ${folder}/${file} ${folder}/Sources/Testing/${file}"
+        git add ${folder}/Sources/Testing/${fp}
+        git mv ${folder}/${file} ${folder}/Sources/Testing/${file}
      elif [ -z "${file##*[M|m]ock*.swift}" ]; then
         if ! [ -d "${folder}/Sources/Testing/${fp}" ]; then
-        : #   mkdir ${folder}/Sources/Testing/${fp}
+           mkdir -p ${folder}/Sources/Testing/${fp}
         fi
-        : #git add ${folder}/Sources/Testing/${fp}
-        : #git mv ${folder}/${file} ${folder}/Sources/Testing/${file}
+        git add ${folder}/Sources/Testing/${fp}
+        git mv ${folder}/${file} ${folder}/Sources/Testing/${file}
      elif [ -z "${file##*SnapshotTests.swift}" ]; then
         if ! [ -d "${folder}/Tests/SnapshotTests/${fp}" ]; then
-           : #mkdir ${folder}/Tests/SnapshotTests/${fp}
+           mkdir -p ${folder}/Tests/SnapshotTests/${fp}
         fi
-        : #git add ${folder}/Tests/SnapshotTests/${fp}
-        : #git mv ${folder}/${file} ${folder}/Tests/SnapshotTests/${file}
+        git add ${folder}/Tests/SnapshotTests/${fp}
+        git mv ${folder}/${file} ${folder}/Tests/SnapshotTests/${file}
      elif [ -z "${file##*Tests.swift}" ]; then
         if ! [ -d "${folder}/Tests/UnitTests/${fp}" ]; then
-           : #mkdir ${folder}/Tests/UnitTests/${fp}
+           mkdir -p ${folder}/Tests/UnitTests/${fp}
         fi
-        : #git add ${folder}/Tests/UnitTests/${fp}
-        : #git mv ${folder}/${file} ${folder}/Tests/UnitTests/${file}
+        git add ${folder}/Tests/UnitTests/${fp}
+        git mv ${folder}/${file} ${folder}/Tests/UnitTests/${file}
      else
         if ! [ -d "${folder}/Sources/Core/${fp}" ]; then
-           : #mkdir ${folder}/Sources/Core/${fp}
+           mkdir -p ${folder}/Sources/Core/${fp}
         fi
-        : #git add ${folder}/Sources/Core/${fp}
-        : #git mv ${folder}/${file} ${folder}/Sources/Core/${file}
+        git add ${folder}/Sources/Core/${fp}
+        git mv ${folder}/${file} ${folder}/Sources/Core/${file}
      fi
    fi
 }
 
 cd ./core/Sources/Components/
 
-for component in Tag*; do
+for component in *; do
   for f in ${component}/*; do
      pp "$f"
   done
