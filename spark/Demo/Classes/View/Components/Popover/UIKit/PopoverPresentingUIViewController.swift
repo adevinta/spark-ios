b2bc5840d1f54a4009052789a5bdc5151b6b4f22
louis.borlee <louis.borlee@adevinta.com>
2024-06-27 09:43:54 +0200

    [Popover#903] Upgraded demo

--- spark/Demo/Classes/View/Components/Popover/UIKit/PopoverPresentingUIViewController.swift
+++ spark/Demo/Classes/View/Components/Popover/UIKit/PopoverPresentingUIViewController.swift
@@ -11,29 +11,45 @@ import SparkCore
 
 final class PopoverPresentingUIViewController: UIViewController {
 
+    private let theme = SparkTheme()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = .systemBackground
-        let button = UIButton(configuration: .filled())
-        button.setTitle("Show Popover", for: .normal)
-        button.translatesAutoresizingMaskIntoConstraints = false
-        self.view.addSubview(button)
-        NSLayoutConstraint.activate([
-            button.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
-            button.centerYAnchor.constraint(equalTo: self.view.centerYAnchor)
-        ])
 
-        button.addAction(.init(handler: { [weak self] _ in
-            guard let self else { return }
-            if let presentedViewController {
-                presentedViewController.dismiss(animated: true)
-            } else {
-                let theme = SparkTheme()
-                let intent = PopoverIntent.main
-                let popoverViewController = PopoverViewController(contentViewController: PopoverContentDemoViewController(theme: theme, intent: intent), theme: theme, intent: intent, showArrow: true)
-                self.presentPopover(popoverViewController, sourceView: button)
-            }
-        }), for: .touchUpInside)
+        let buttons: [UIView] = PopoverIntent.allCases.enumerated().map { index, intent in
+            let popoverColors = intent.getColors(theme: self.theme)
+            let button = UIButton(configuration: .filled())
+            button.setTitle(intent.name, for: .normal)
+            button.setTitleColor(popoverColors.foreground.uiColor, for: .normal)
+            button.tintColor = popoverColors.background.uiColor
+            button.addAction(.init(handler: { [weak self] _ in
+                self?.showPopover(sourceView: button, intent: intent, withArrow: index % 2 == 0)
+            }), for: .touchUpInside)
+            return button
+        }
+
+        let stackView = UIStackView(arrangedSubviews: buttons)
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        stackView.alignment = .center
+        stackView.spacing = 12
+
+        self.view.addSubview(stackView)
+        NSLayoutConstraint.activate([
+            stackView.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+            stackView.centerYAnchor.constraint(equalTo: self.view.centerYAnchor)
+        ])
+    }
+
+    private func showPopover(sourceView: UIView, intent: PopoverIntent, withArrow showArrow: Bool) {
+        if let presentedViewController {
+            presentedViewController.dismiss(animated: true)
+        } else {
+            let theme = SparkTheme()
+            let popoverViewController = PopoverViewController(contentViewController: PopoverContentDemoViewController(theme: theme, intent: intent), theme: theme, intent: intent, showArrow: showArrow)
+            self.presentPopover(popoverViewController, sourceView: sourceView)
+        }
     }
 }
 
